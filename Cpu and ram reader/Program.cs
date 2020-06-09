f42e48fd9486e3be8a4bc1a7f0ecf28284ef697b
jesper Klejs Deleurand <Jesperd55@gmail.com>
2020-06-09 08:49:55 +0200

    Initial commit

---
+++ Cpu and ram reader/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Cpu_and_ram_reader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunTest();
+        }
+
+        private static void RunTest()
+        {
+            bool done = false;
+            PerformanceCounter cpuPercent = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+            PerformanceCounter ramUsed = new PerformanceCounter("Memory", "Available MBytes");
+            while (!done)
+            {
+                float t = cpuPercent.NextValue();
+                float r = ramUsed.NextValue();
+                Console.WriteLine(String.Format("CPU: " + t + "%"));
+                Console.WriteLine(string.Format("RAM: " + r + "MB"));
+                System.Threading.Thread.Sleep(1000);
+                Console.WriteLine();
+            }
+        }
+    }
+}
